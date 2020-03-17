b226a99fa9a170399f9bd520fb048d24296e5ad4
Selena Li <sandy460544826@gmail.com>
2020-03-17 23:00:51 +0000

    Login/Register Feature Completed

--- backend/app.py
+++ backend/app.py
@@ -5,19 +5,23 @@ import MySQLdb.cursors
 from ebaysdk.finding import Connection
 
 app = Flask(__name__)
-
+app.secret_key = 'COMP0022'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'test'
 app.config['MYSQL_PASSWORD'] = 'test'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'COMP0022'
 
 # Intialize MySQL
 mysql = MySQL(app)
-
 @app.route('/')
-def main_page():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('index.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,12 +42,58 @@ def login():
             session['id'] = account['id']
             session['username'] = account['username']
             # Redirect to home page
-            return 'Logged in successfully!'
+            return redirect(url_for('home'))
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('login.html', msg=msg)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
+        postcode = request.form['postcode']
+        country = request.form['country']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', [username])
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not re.match(r'[ A-Za-z0-9]+', address):
+            msg = 'Address must contain only characters and numbers!'
+        elif not re.match(r'[A-Za-z0-9]+', postcode):
+            msg = 'Postcode must contain only characters and numbers!'
+        elif not re.match(r'[ A-Za-z]+', country):
+            msg = 'Country must contain only characters and numbers!'
+        elif not re.match(r'[0-9]+', phone):
+            msg = 'Phone number must contain only numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s, %s, %s)', [username, password, email, phone, address, postcode, country])
+            mysql.connection.commit()
+            return redirect(url_for('home'))
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
 
 @app.route('/logout')
 def logout():
@@ -51,8 +101,8 @@ def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('index'))
+   # Redirect to main page
+   return redirect(url_for('home'))
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -77,6 +127,18 @@ def search():
 
     return 'Hello World!'
 
+@app.route('/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
--- createTable.sql
+++ createTable.sql
@@ -1,12 +1,16 @@
-CREATE DATABASE IF NOT EXISTS `pythonlogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pythonlogin`;
+CREATE DATABASE IF NOT EXISTS `COMP0022` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `COMP0022`;
 
 CREATE TABLE IF NOT EXISTS `accounts` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
   	`username` varchar(50) NOT NULL,
   	`password` varchar(255) NOT NULL,
   	`email` varchar(100) NOT NULL,
+  	`phone` varchar(15) NOT NULL,
+  	`address` varchar(100) NOT NULL,
+  	`postcode` varchar(10) NOT NULL,
+  	`country` varchar(50) NOT NULL DEFAULT 'United Kingdom',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `phone`, `address`, `postcode`, `country`) VALUES (1, 'test', 'test', 'test@test.com', '07511111111', 'UCL', 'WC1E6BT', 'United Kingdom');
